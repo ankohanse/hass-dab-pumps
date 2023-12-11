8bdf4ef8c7b5d43f3612b3554b6df06890349679
Anko Hanse <anko_hanse@hotmail.com>
2023-12-11 22:48:59 +1300

    Correct typo in folder name custom_components

---
+++ custom_components/dabpumps/__init__.py
@@ -0,0 +1,86 @@
+"""__init__.py: The DAB Pumps integration."""
+from __future__ import annotations
+
+import logging
+import json
+import voluptuous as vol
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import (device_registry as dr, entity_registry)
+from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
+from homeassistant.helpers.translation import async_get_translations
+
+from homeassistant.const import (
+    CONF_USERNAME,
+    CONF_PASSWORD,
+)
+
+from .dabpumpsapi import DabPumpsApi
+from .coordinator import DabPumpsCoordinator
+
+from .const import (
+    DOMAIN,
+    HUB,
+    COORDINATOR,
+    STARTUP_MESSAGE,
+    DEFAULT_USERNAME,
+    DEFAULT_PASSWORD
+)
+
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.info(STARTUP_MESSAGE)
+
+
+PLATFORMS: list[Platform] = [
+    Platform.SENSOR
+]
+
+
+#async def async_setup(hass, config):
+#    """Set up DAB Pumps components."""
+#    hass.data.setdefault(DOMAIN, {})
+#
+#    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Set up DAB Pumps from a config entry."""
+    
+    # Setup DOMAIN as default
+    hass.data.setdefault(DOMAIN, {})
+    
+    # Store an instance of the DabPumpsApi instance in hass.data[domain]
+    options = config_entry.data.get('options', {})
+    username = config_entry.data[CONF_USERNAME]
+    password = config_entry.data[CONF_PASSWORD]
+
+    dabpumps_api = DabPumpsApi(username, password)
+    coordinator = DabPumpsCoordinator(hass, dabpumps_api, options)
+
+    hass.data[DOMAIN][HUB] = dabpumps_api
+    hass.data[DOMAIN][COORDINATOR] = coordinator
+
+    # Fetch initial data so we have data when entities subscribe
+    #
+    # If the refresh fails, async_config_entry_first_refresh will
+    # raise ConfigEntryNotReady and setup will try again later
+    #
+    await coordinator.async_config_entry_first_refresh()
+    
+    # Forward to all platforms (sensor, switch, ...)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    
+    return True
+
+
+async def async_update_options(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Update options."""
+    await hass.config_entries.async_reload(config_entry.entry_id)
+
+    
+async def options_update_listener(hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry):
+    """Handle options update."""
+    await hass.config_entries.async_reload(config_entry.entry_id)
